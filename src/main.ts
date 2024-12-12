4d51efb3d21e73b1b0b4c8b191cd39d66a03e958
Mykhailo Vlasov <mykhaylo.vlasov@intelliboard.net>
2024-12-12 10:15:01 +0100

    finish

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
     const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
         transport: Transport.GRPC,
         options: {
-            url: '0.0.0.0:5000',
-            package: 'payment',
-            protoPath: join(__dirname, './payment/payment.proto'),
+            url: process.env.GRPC_URL || '0.0.0.0:5000',
+            package: process.env.GRPC_PACKAGE || 'payment',
+            protoPath: join(__dirname, process.env.PROTO_PATH || './payment/payment.proto'),
         },
     });
 
